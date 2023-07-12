f9bba29aff50958ffdd8e6ef859761231a5e75da
testgithubbi <123469328+testgithubbi@users.noreply.github.com>
2023-07-13 00:50:20 +0530

    1

---
+++ task1/js/main.js
@@ -0,0 +1,24 @@
+function show() {
+	document.querySelector(".dropdown-1").style.visibility="visible"
+	document.querySelector(".dropdown-1").style.opacity="1"
+}
+function show1() {
+	document.querySelector(".dropdown-2").style.visibility="visible"
+	document.querySelector(".dropdown-2").style.opacity="1"
+}
+function show2() {
+	document.querySelector(".dropdown-3").style.visibility="visible"
+	document.querySelector(".dropdown-3").style.opacity="1"
+}
+function show3() {
+	document.querySelector(".dropdown-4").style.visibility="visible"
+	document.querySelector(".dropdown-4").style.opacity="1"
+}
+function show4() {
+	document.querySelector(".dropdown-5").style.visibility="visible"
+	document.querySelector(".dropdown-5").style.opacity="1"
+}
+function show5() {
+	document.querySelector(".dropdown-6").style.visibility="visible"
+	document.querySelector(".dropdown-6").style.opacity="1"
+}
